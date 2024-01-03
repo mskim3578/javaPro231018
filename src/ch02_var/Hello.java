4084e0b563e65e8463981d2ef0046076cb42e14d
mskim3578 <mskim3578@naver.com>
2024-01-03 16:21:40 +0900

    aaaa

--- src/ch02_var/Hello.java
+++ src/ch02_var/Hello.java
@@ -1,9 +1,23 @@
 package ch02_var;
 
+import java.util.Arrays;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
+		name1("aaa", "a","b","c");
+		name2("aaa", "a","b","c");
+	}
+	
+	public static void name1(String ...b) {
+		System.out.println(Arrays.asList(b));
+		
+	}
+	
+	public static void name2(String a, String ...b) {
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
